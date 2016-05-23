dc01d5afe272cc7ed8bca8956adaeb166634a172
Mikhail Vaysman <mikhail.vaysman@gmail.com>
2016-05-24 00:35:02 +0200

    add header and rewrite some string

--- http_proxy/http_proxy.go
+++ http_proxy/http_proxy.go
@@ -5,16 +5,45 @@ import (
 	"github.com/elazarl/goproxy"
 	jww "github.com/spf13/jwalterweatherman"
 	"net/http"
+	"io/ioutil"
+	"bytes"
+	"regexp"
 )
 
 func Run(viper *viper.Viper) {
+	urls := map[string]string{
+		"Accept-Encoding": "_Accept-Encoding_",
+		"Host": "_Host_",
+	}
+
 	jww.INFO.Println("Start HTTP rewriter")
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
+	proxy.OnRequest(goproxy.ReqHostMatches(regexp.MustCompile("^.*\\.org:.*$"))).HandleConnect(goproxy.AlwaysMitm)
+	proxy.OnRequest().DoFunc(func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+		r.Header.Set("X-GoProxy", "yxorPoG-X")
+		return r, nil
+	})
 	proxy.OnResponse(goproxy.DstHostIs("httpbin.org")).DoFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
 		jww.INFO.Printf("Response %v\tContext %v", resp, ctx)
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			jww.ERROR.Printf("Error: %v URL: %v", err, ctx.Req.URL)
+			return goproxy.TextResponse(ctx.Req, "Error getting body")
+		}
+		err = resp.Body.Close()
+		if err != nil {
+			jww.ERROR.Printf("Error closing %v: %v", ctx.Req.URL, err)
+			return goproxy.TextResponse(ctx.Req, "Error clossing body")
+		}
+		for old, new := range urls {
+			b = bytes.Replace(b, []byte(old), []byte(new), -1)
+		}
+		body := ioutil.NopCloser(bytes.NewReader(b))
+		resp.Body = body
+
 		return resp
 	})
-	proxy.OnRequest().HijackConnect()
+
 	jww.FATAL.Fatal(http.ListenAndServe(":8080", proxy))
 }
